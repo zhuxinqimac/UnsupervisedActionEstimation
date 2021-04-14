0c109d918c51889efe13549033ca29b0ff6b91fc
zhuxinqimac <zhuxinqimac@yahoo.com>
2021-04-14 23:47:30 +1000

    uneven fac vae

--- models/uneven_facvae.py
+++ models/uneven_facvae.py
@@ -8,7 +8,7 @@
 
 # --- File Name: uneven_facvae.py
 # --- Creation Date: 13-04-2021
-# --- Last Modified: Tue 13 Apr 2021 22:50:41 AEST
+# --- Last Modified: Wed 14 Apr 2021 23:47:21 AEST
 # --- Author: Xinqi Zhu
 # .<.<.<.<.<.<.<.<.<.<.<.<.<.<.<.<
 """
@@ -33,14 +33,15 @@ from logger.imaging import ShowRecon, LatentWalk, ReconToTb
 class UnevenFacVAE(UnevenVAE):
     def __init__(self, args):
         super().__init__(args)
-        self.discriminator = Discriminator(args.latents)
-        self.disc_opt = optim.Adam(self.discriminator.parameters(), lr=1e-4, betas=(0.5, 0.9))
-        self.gamma = float(args.factor_vae_gamma) if args.factor_vae_gamma is not None else 6.4
+        self.discriminator = [Discriminator(latents).cuda()]  # Exclude from register.
+        self.disc_opt = optim.Adam(self.discriminator[0].parameters(), lr=1e-4, betas=(0.5, 0.9))
+        # self.disc_opt = optim.Adam(self.discriminator.parameters(), lr=1e-4)
+        self.gamma = float(gamma) if gamma is not None else 6.4
 
     def permute_dims(self, z):
         assert z.dim() == 2
 
-        z = z.permute(1, 0)
+        # z = z.permute(1, 0)
         B, _ = z.size()
         perm_z = []
         for z_j in z.split(1, 1):
@@ -48,25 +49,26 @@ class UnevenFacVAE(UnevenVAE):
             perm_z_j = z_j[perm]
             perm_z.append(perm_z_j)
 
-        return torch.cat(perm_z, 1).permute(1, 0)
+        # return torch.cat(perm_z, 1).permute(1, 0)
+        return torch.cat(perm_z, 1)
 
     def main_step(self, batch, batch_nb, loss_fn):
         out = super().main_step(batch, batch_nb, loss_fn)
         state = out['state']
         x, y, mu, lv, z, x_hat = state['x'], state['y'], state['mu'], state['lv'], state['z'], state['x_hat']
 
-        D_z = self.discriminator(z.detach())
+        D_z = self.discriminator[0](z.detach())
         z_perm = self.permute_dims(z)
-        D_z_perm = self.discriminator(z_perm.detach())
-        D_tc_loss = 0.5 * (F.cross_entropy(D_z, torch.zeros(D_z.shape[0]).to(D_z.device).long())
-                           + F.cross_entropy(D_z_perm, torch.ones(D_z.shape[0]).to(D_z.device).long()))
+        D_z_perm = self.discriminator[0](z_perm.detach())
+        D_tc_loss = 0.5 * (F.cross_entropy(D_z, torch.zeros(D_z.shape[0], dtype=torch.long).to(D_z.device))
+                           + F.cross_entropy(D_z_perm, torch.ones(D_z.shape[0], dtype=torch.long).to(D_z.device)))
 
         if self.training:
             self.disc_opt.zero_grad()
             D_tc_loss.backward()
             self.disc_opt.step()
 
-        D_z = self.discriminator(z)
+        D_z = self.discriminator[0](z)
         vae_loss = out['loss']
         vae_tc_loss = (D_z[:, :1] - D_z[:, 1:]).mean() * self.gamma
 
